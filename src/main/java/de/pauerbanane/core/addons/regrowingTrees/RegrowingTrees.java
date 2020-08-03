34e488081649493b07ecea4bc59672d6b6fac5bb
PauerBanane <58947823+PauerBanane@users.noreply.github.com>
2020-08-03 23:29:20 +0200

    initial commit

---
+++ src/main/java/de/pauerbanane/core/addons/regrowingTrees/RegrowingTrees.java
@@ -0,0 +1,21 @@
+package de.pauerbanane.core.addons.regrowingTrees;
+
+import de.pauerbanane.api.addons.Addon;
+
+public class RegrowingTrees extends Addon {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onReload() {
+
+    }
+}
--- src/main/java/de/pauerbanane/core/commands/CustomItemCommand.java
+++ src/main/java/de/pauerbanane/core/commands/CustomItemCommand.java
@@ -1,12 +1,10 @@
 package de.pauerbanane.core.commands;
 
 import de.pauerbanane.acf.BaseCommand;
-import de.pauerbanane.acf.annotation.CommandAlias;
-import de.pauerbanane.acf.annotation.CommandCompletion;
-import de.pauerbanane.acf.annotation.CommandPermission;
-import de.pauerbanane.acf.annotation.Default;
+import de.pauerbanane.acf.annotation.*;
 import de.pauerbanane.api.util.F;
 import de.pauerbanane.api.util.ItemBuilder;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -17,8 +15,8 @@ public class CustomItemCommand extends BaseCommand {
 
     @Default
     @CommandCompletion("@material @nothing")
-    public void get(Player sender, Material material, int modelData) {
-        ItemStack item = new ItemBuilder(material).setModelData(modelData).build();
+    public void get(Player sender, Material material, String name, @Optional int modelData) {
+        ItemStack item = new ItemBuilder(material).name(ChatColor.translateAlternateColorCodes('&', name)).setModelData(modelData).build();
         sender.getInventory().addItem(item);
         sender.sendMessage(F.main("Item", "Das Item wurde erstellt."));
     }
